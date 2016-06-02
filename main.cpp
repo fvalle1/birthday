d1b21fecd88740b7e2a52418494702cb22b268d7
Filippo <mail@fvalle.me>
2016-06-02 10:32:27 +0200

    add part II

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@ using namespace std;
 double seed=33149495;
 
 int main(){
+	//I experiment
   //open file
   ofstream save;
   save.open("montecarlo.csv", ios::out);
@@ -17,7 +18,7 @@ int main(){
   //file
   srand(seed);  
 int year[365]={0};
- int people=55,n=100000;
+ int people=55,n=500000;
 int cntr=0;
  for(int i=2;i<=people;i++){//table per people
    cntr=0;
@@ -29,5 +30,22 @@ int cntr=0;
    cout<<i<<" people\tp:  "<<setw(6)<<(double)cntr/n*100<<"%"<<endl;
    save<<i<<";"<<(double)cntr/n<<endl; 
 }
+	//II experiment
+	
+	double average=0;
+	
+	for(int j=1; j<n;j++){//n simulations
+		for(int l=0;l<365;l++) year[l]=0; //reset
+		cntr=0;
+		
+	while(true){
+		cntr++;
+		if(++year[rand()%365]==2) break;	 //count how many people before two have same birthday
+	}
+		average=((average*(j-1))+cntr)/j;
+	}
+	cout<<"\n\n***************************\n\n";
+	cout<<"On average with "<<n<<" simulation, I need "<<average<<" people to be sure two have same birthday\n\n";
+	
   return 0;
 }
